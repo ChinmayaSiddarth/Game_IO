72874b067dcb36c957664f1d3f9bb34160f883ef
ChinmayaSiddarth <53006123+ChinmayaSiddarth@users.noreply.github.com>
2021-01-07 23:52:23 +0530

    added all the files
    still koushik is the boss

---
+++ gamer_zone/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from gamer_zone.models import Food,Drink,Indoor_Game,Outdoor_Game
+
+# Register your models here.
+
+admin.site.register(Food)
+admin.site.register(Drink)
+admin.site.register(Indoor_Game)
+admin.site.register(Outdoor_Game)
